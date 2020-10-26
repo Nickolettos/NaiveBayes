7a1860aea4dd2fd701ddf82ef88cfeda0bea5fe6
Dayton-His-Glory-Alone <45398901+Dayton-His-Glory-Alone@users.noreply.github.com>
2020-10-26 14:19:02 -0500

    Print file contents to console
    Next: Need to store classification for each line

--- NB.java
+++ NB.java
@@ -1,4 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class NB {
-
+	
+	public static Scanner scan;
+	public static String classification;
+	
+	public NB() {
+		
+		
+	}
+	public static void parse(String filename) {
+		File file= new File(filename);
+		try {
+			scan= new Scanner(file);
+			while(scan.hasNextLine()) {
+				String line= scan.nextLine();
+				System.out.println(line);
+			}
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		
+		
+	}
+	
+	
 }
