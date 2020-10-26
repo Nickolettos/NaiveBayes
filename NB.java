a93e2370652db0af65ebe766bde00b511faefa0f
Dayton-His-Glory-Alone <45398901+Dayton-His-Glory-Alone@users.noreply.github.com>
2020-10-26 14:47:33 -0500

    Add Classification

--- NB.java
+++ NB.java
@@ -11,19 +11,27 @@ public class NB {
 		
 		
 	}
+	
 	public static void parse(String filename) {
 		File file= new File(filename);
+		//parse differently depending on file type (.meta||.test||.train)
+		//if(filename.substring(filename.valueOf('.')+1).equals(".train")) {
 		try {
 			scan= new Scanner(file);
 			while(scan.hasNextLine()) {
 				String line= scan.nextLine();
-				System.out.println(line);
+				System.out.println(line);//print to console for testing
+				String[]items= line.split(",");
+				classification= items[6];
+				System.out.println("Classication: "+ classification);
 			}
 		} catch (FileNotFoundException e) {
 			
 			e.printStackTrace();
 		}
-		
+		//}
+		//else if (ends with .meta)
+		//else if (ends with .train
 		
 	}
 	
